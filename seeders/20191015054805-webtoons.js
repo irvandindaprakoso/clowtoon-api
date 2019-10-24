4ddd5e1551e0c552199eba32f0f312a222bea215
Irvan Dinda Prakoso <irvandindaprakoso>
2019-10-24 09:24:14 +0700

    edit seeder webtoons

--- seeders/20191015054805-webtoons.js
+++ seeders/20191015054805-webtoons.js
@@ -4,7 +4,7 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('webtoons', [
       {
-        // title: 'One Piece',
+        title: "One Piece",
         genre:'action',
         isFavourite: true,
         url: 'https://cdn.vox-cdn.com/thumbor/7TzXXGeyMRbOjKE7UDiNXOHlQK0=/0x0:1280x738/1200x800/filters:focal(538x267:742x471)/cdn.vox-cdn.com/uploads/chorus_image/image/65232906/one_piece.0.png',
@@ -13,7 +13,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        // title: 'Naruto',
+        title: "Naruto",
         genre:'action',
         isFavourite: true,
         url: 'https://i.pinimg.com/originals/49/2a/23/492a2318346694e8aa786d2a80e6ba33.jpg',
@@ -22,7 +22,7 @@ module.exports = {
         updatedAt: new Date(),
       },
       {
-        // title: 'Nanatsu no Taizai',
+        title: "Nanatsu no Taizai",
         genre:'action',
         isFavourite: true,
         url: 'https://gcube.id/wp-content/uploads/2018/09/gcube-ngr-nanatsu-no-taizai-featured-image-799x500.jpg',
